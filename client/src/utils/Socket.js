eaf7541b74686aa4581794434ff42a6bddaf2a4d
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-05-22 02:30:38 -0700

    changed websocket urls

--- client/src/utils/Socket.js
+++ client/src/utils/Socket.js
@@ -1,5 +1,5 @@
 function Socket(evId, immediateSend, payload) {
-  const url = `ws://localhost:8080/ws?event=${evId}`
+  const url = `ws://ws?event=${evId}`
   this.ws = new WebSocket(url)
 
   this.ws.onopen = () => {
