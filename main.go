4717791b09d7b8c1e7635ec6d126c9b32d0d4198
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-05-26 02:56:31 -0700

    go build

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ func main() {
   router.HandleFunc("/users/load", controllers.LoadUser(models.DBCon)).Methods("POST")
   router.HandleFunc("/completeAuth", auth.CompleteAuth).Methods("GET")
   router.HandleFunc("/ws", serveWs(models.DBCon))
-  router.PathPrefix("/home").Handler(http.FileServer(http.Dir("./client/build")))
+  router.PathPrefix("/").Handler(http.FileServer(http.Dir("./client/build")))
   router.HandleFunc("/", redirect(url))
   // router.PathPrefix("/").Handler(http.FileServer(http.Dir("./client/build")))
   http.FileServer(http.Dir("./client/build"))
