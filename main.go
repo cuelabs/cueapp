97834081675d5bab317cb0801118c56e1bf32aef
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-05-26 19:04:51 -0700

    methods get

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ func main() {
   router.HandleFunc("/events/guests", controllers.ReadAllUsersEvent(models.DBCon)).Methods("POST")
   router.HandleFunc("/users/create", controllers.CreateUser(models.DBCon)).Methods("POST")
   router.HandleFunc("/users/load", controllers.LoadUser(models.DBCon)).Methods("POST")
-  router.HandleFunc("/callback", auth.CompleteAuth)
+  router.HandleFunc("/callback", auth.CompleteAuth).Methods("GET")
   router.HandleFunc("/ws", serveWs(models.DBCon))
   router.PathPrefix("/").Handler(http.FileServer(http.Dir("./client/build")))
   router.HandleFunc("/", redirect(url))
