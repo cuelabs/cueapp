64d42583e6505bd1ba0f2c15aaa4b1bca4eeb694
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-15 18:46:39 -0700

    trying to get client

--- spotify_search.go
+++ spotify_search.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "github.com/mattcarpowich1/cueapp/models"
+  // "github.com/zmb3/spotify"
   "net/http"
   "encoding/json"
   "fmt"
@@ -19,6 +20,23 @@ func Search(h *SpotifyHub) http.HandlerFunc {
 
     fmt.Println(q.SUID)
     fmt.Println(q.Query)
+
+    fmt.Println("1")
+
+    client := h.clients[q.SUID]
+    fmt.Printf("%+v\n", client)
+
+    fmt.Println("2")
+
+    // result, err := client.Search(q.Query, SearchTypeTrack)
+    // if err != nil {
+    //   fmt.Println("3")
+    //   panic(err)
+    // }
+
+    // fmt.Printf("%+v\n", result)
+
+    fmt.Println("4")
     
     w.WriteHeader(http.StatusOK)
   }
