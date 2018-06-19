12118ca53bb5d08163f7d7435db27e9bcc61d8b2
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-18 18:29:24 -0700

    play stuff in player

--- client/src/pages/EventHost/components/Player.js
+++ client/src/pages/EventHost/components/Player.js
@@ -1,6 +1,31 @@
 import React from 'react'
 
 const Player = props => {
+  const play = ({
+      spotify_uri,
+      playerInstance: {
+        _options: {
+          getOAuthToken,
+          id
+        }
+      }
+    }) => {
+      getOAuthToken(access_token => {
+        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+          method: 'PUT',
+          body: JSON.stringify({ uris: [spotify_uri] }),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${access_token}`
+          },
+        });
+      });
+    };
+
+    play({
+      playerInstance: new window.Spotify.Player({ name: "ayy" }),
+      spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
+    });
   return (
     <div></div>
   )
--- client/src/pages/EventHost/index.js
+++ client/src/pages/EventHost/index.js
@@ -133,32 +133,6 @@ class EventHost extends Component {
         }
       })
 
-    const play = ({
-      spotify_uri,
-      playerInstance: {
-        _options: {
-          getOAuthToken,
-          id
-        }
-      }
-    }) => {
-      getOAuthToken(access_token => {
-        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
-          method: 'PUT',
-          body: JSON.stringify({ uris: [spotify_uri] }),
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${access_token}`
-          },
-        });
-      });
-    };
-
-    play({
-      playerInstance: new window.Spotify.Player({ name: "ayy" }),
-      spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
-    });
-
     return !eventLoading ? (
       <div className='page event-host'>
         <h2 className='host-view-event-title'>
