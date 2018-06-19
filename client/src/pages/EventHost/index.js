49d8cbd3ea31fea0b16d0fef999d49913dce2106
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-18 19:56:58 -0700

    trying different way of PUTting

--- client/src/pages/EventHost/index.js
+++ client/src/pages/EventHost/index.js
@@ -69,31 +69,19 @@ class EventHost extends Component {
   }
 
   handleScriptLoad() {
-    const play = ({
-      spotify_uri,
-      playerInstance: {
-        _options: {
-          getOAuthToken,
-          id
-        }
-      }
-    }) => {
-      getOAuthToken(access_token => {
-        fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
-          method: 'PUT',
-          body: JSON.stringify({ uris: [spotify_uri] }),
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${access_token}`
-          },
-        });
-      });
-    };
 
-    play({
-      playerInstance: window.myPlayer,
-      spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
+    const { token } = this.props
+    console.log('here is a token :', token)
+    
+    fetch(`https://api.spotify.com/v1/me/player/play`, {
+      method: 'PUT',
+      body: JSON.stringify({ uris: ['spotify:track:49zD0wr2S3d0lZPib0K4e1'] }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
     });
+
   }
 
   handleRequest (uid, accept) {
