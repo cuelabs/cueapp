f7d11b0a063a27dee80c729631cfe3fbcead7320
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-18 20:31:15 -0700

    future

--- client/src/pages/EventHost/index.js
+++ client/src/pages/EventHost/index.js
@@ -86,10 +86,10 @@ class EventHost extends Component {
       getOAuthToken(access_token => {
         fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
           method: 'PUT',
-          body: JSON.stringify({ uris: ['spotify:track:49zD0wr2S3d0lZPib0K4e1'] }),
+          body: JSON.stringify({ uris: [spotify_uri] }),
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${access_token}`
+            'Authorization': `Bearer ${token}`
           },
         });
       });
