721212b725b19c5988369f58a071d47888caccf9
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-06-27 11:07:48 -0700

    in case someone views my react code, comment out fake data, will refactor front end in a bit

--- client/src/pages/EventHost/components/GuestList.js
+++ client/src/pages/EventHost/components/GuestList.js
@@ -13,37 +13,37 @@ class GuestList extends Component {
 
   render () {
     const { guests, eventId, userId } = this.props
-    // const users = guests
-    //   .filter(g => {
-    //     if ((g.EventID === eventId) && g.IsActive && (g.UserID !== userId)) {
-    //       return true
-    //     } else {
-    //       return false
-    //     }
-    //   })
-    const users = [
-      {
-        UserID: 1,
-        SUID: '123416101',
-        DisplayName: 'Matt Carpowich',
-        DisplayImage: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/32169476_10215922716037991_3090206953469640704_n.jpg?_nc_cat=0&oh=40670caaf299735497d47929a818e7c5&oe=5B81B0BD'
-      }, {
-        UserID: 2,
-        SUID: '123416101',
-        DisplayName: 'Matthew Carpowich',
-        DisplayImage: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/32169476_10215922716037991_3090206953469640704_n.jpg?_nc_cat=0&oh=40670caaf299735497d47929a818e7c5&oe=5B81B0BD'
-      }, {
-        UserID: 3,
-        SUID: '123416101',
-        DisplayName: 'Matty Carp',
-        DisplayImage: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/32169476_10215922716037991_3090206953469640704_n.jpg?_nc_cat=0&oh=40670caaf299735497d47929a818e7c5&oe=5B81B0BD'
-      }, {
-        UserID: 4,
-        SUID: '123416101',
-        DisplayName: 'Mattincredible',
-        DisplayImage: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/32169476_10215922716037991_3090206953469640704_n.jpg?_nc_cat=0&oh=40670caaf299735497d47929a818e7c5&oe=5B81B0BD'
-      }
-    ]
+    const users = guests
+      .filter(g => {
+        if ((g.EventID === eventId) && g.IsActive && (g.UserID !== userId)) {
+          return true
+        } else {
+          return false
+        }
+      })
+    // const users = [
+    //   {
+    //     UserID: 1,
+    //     SUID: '123416101',
+    //     DisplayName: 'Matt Carpowich',
+    //     DisplayImage: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/32169476_10215922716037991_3090206953469640704_n.jpg?_nc_cat=0&oh=40670caaf299735497d47929a818e7c5&oe=5B81B0BD'
+    //   }, {
+    //     UserID: 2,
+    //     SUID: '123416101',
+    //     DisplayName: 'Matthew Carpowich',
+    //     DisplayImage: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/32169476_10215922716037991_3090206953469640704_n.jpg?_nc_cat=0&oh=40670caaf299735497d47929a818e7c5&oe=5B81B0BD'
+    //   }, {
+    //     UserID: 3,
+    //     SUID: '123416101',
+    //     DisplayName: 'Matty Carp',
+    //     DisplayImage: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/32169476_10215922716037991_3090206953469640704_n.jpg?_nc_cat=0&oh=40670caaf299735497d47929a818e7c5&oe=5B81B0BD'
+    //   }, {
+    //     UserID: 4,
+    //     SUID: '123416101',
+    //     DisplayName: 'Mattincredible',
+    //     DisplayImage: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/32169476_10215922716037991_3090206953469640704_n.jpg?_nc_cat=0&oh=40670caaf299735497d47929a818e7c5&oe=5B81B0BD'
+    //   }
+    // ]
     return (
       <div className='view'>
         <ul className='host-view-page-list guests'>
